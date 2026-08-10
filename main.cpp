0adeb77fd0db799bb89dd29457bfac53888109a9
dasha <perowa.d.m@gmail.com>
2026-08-10 18:22:02 +0300

    cheese

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Supermassive Black Hole");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
